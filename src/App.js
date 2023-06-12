d522d0887cc296db63087fb86d39da6e85c6f9a9
kiran <kirankiru3434@gmail.com>
2023-06-12 18:59:32 +0530

    added meals summary and meals item list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from "./Components/Layout/Header";
 import React from "react";
+import AvailableMeals from "./Components/Meals/AvailableMeals";
+import MealsSummary from "./Components/Meals/MealsSummary";
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <MealsSummary />
+        <AvailableMeals></AvailableMeals>
+      </main>
     </React.Fragment>
   );
 }
