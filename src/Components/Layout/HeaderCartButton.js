1d75f8e234e2aa5be3744d1f4651a5d094a8153e
kiran <kirankiru3434@gmail.com>
2023-06-13 12:55:05 +0530

    added remove item and add item functionality in carditems modal

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,21 +1,45 @@
 import classes from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
-const HeaderCartbutton = (props) => {
-  const cardCtx = useContext(CartContext);
-  const numberofCartItems = cardCtx.items.reduce((curNumber, item) => {
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
-        <CartIcon></CartIcon>
+        <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{numberofCartItems}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
 
-export default HeaderCartbutton;
+export default HeaderCartButton;
